05b240d58f502000a71c384aa310245ea96e8f52
carteblanchecarriage <115643776+carteblanchecarriage@users.noreply.github.com>
2022-12-08 17:24:40 -0500

    Combined Loops

--- app.js
+++ app.js
@@ -3,61 +3,35 @@ let h2 = document.querySelectorAll('h2');
 let p = document.querySelectorAll('p');
 let div1 = document.getElementById('div1')
 let div2 = document.getElementById('div2')
+const nodeArray = [p, h1, h2];
 
 /* Find all the letters 'e', replace them with a new span element and make them a random color. 
 Make the background color of the body the opposite/complimentary color*/
+
 function searchAndColor() {
-    const a = Math.floor(Math.random() * 255)
-    const b = Math.floor(Math.random() * 255)
-    const c = Math.floor(Math.random() * 255)
+    const a = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const c = Math.floor(Math.random() * 255);
     let rgbValue = `rgb(${a}, ${b}, ${c})`;
     let rgbValue2 = `rgb(${255 - a}, ${255 - b}, ${255 - c})`;
 
-    p.forEach(function (node) {
-        let searchString = node.innerText;
-        node.innerText = '';
-        for (let i = 0; i < searchString.length; i++) {
-            if (searchString[i].toLowerCase() === 'e') {
-                let spanLetter = document.createElement('span');
-                spanLetter.innerHTML = searchString[i];
-                node.append(spanLetter);
-                spanLetter.style.color = rgbValue;
-            } else {
-                node.innerHTML += searchString[i]
+    for (let z = 0; z < nodeArray.length; z++) {
+        nodeArray[z].forEach(function (node) {
+            let searchString = node.innerText;
+            node.innerText = '';
+            for (let i = 0; i < searchString.length; i++) {
+                if (searchString[i].toLowerCase() === 'e') {
+                    let spanLetter = document.createElement('span');
+                    spanLetter.innerHTML = searchString[i];
+                    node.append(spanLetter);
+                    spanLetter.style.color = rgbValue;
+                } else {
+                    node.innerHTML += searchString[i]
+                }
             }
-        }
-        document.body.style.backgroundColor = rgbValue2;
-    })
-
-    h1.forEach(function (node) {
-        let searchString = node.innerText;
-        node.innerText = '';
-        for (let i = 0; i < searchString.length; i++) {
-            if (searchString[i].toLowerCase() === 'e') {
-                let spanLetter = document.createElement('span');
-                spanLetter.innerHTML = searchString[i];
-                node.append(spanLetter);
-                spanLetter.style.color = rgbValue;
-            } else {
-                node.innerHTML += searchString[i]
-            }
-        }
-    })
-
-    h2.forEach(function (node) {
-        let searchString = node.innerText;
-        node.innerText = '';
-        for (let i = 0; i < searchString.length; i++) {
-            if (searchString[i].toLowerCase() === 'e') {
-                let spanLetter = document.createElement('span');
-                spanLetter.innerHTML = searchString[i];
-                node.append(spanLetter);
-                spanLetter.style.color = rgbValue;
-            } else {
-                node.innerHTML += searchString[i]
-            }
-        }
-    })
+            document.body.style.backgroundColor = rgbValue2;
+        })
+    };
 
     div1.style.backgroundColor = rgbValue;
     div2.style.backgroundColor = rgbValue2;
